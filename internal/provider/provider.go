e7a7231fead8346adb72bf58de878a6341efd177
Jasmine <110918682+jsminem@users.noreply.github.com>
2025-11-12 09:51:41 -0500

    INS-60299: [Terraform]: Automate Registration for AWS RDS MySQL - UC (#16)
    Signed-off-by: Jasmine Multani <jasmine@ibm.com>
    Signed-off-by: Supraja Vempalli <suprajavempalli@ibm.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -60,6 +60,7 @@ func (p *GDPMiddlewareHelperProvider) Resources(ctx context.Context) []func() re
 		NewExecuteAwsLambdaFunctionResource,
 		NewRDSRebootResource,
 		NewAuroraRebootResource,
+		NewRDSModifyResource,
 	}
 }
 
